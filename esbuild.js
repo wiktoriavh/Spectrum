70fffbb663cf40120cbbbe106e28dedb77a1460b
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-03-13 11:37:23 +0100

    🔨 build function

---
+++ esbuild.js
@@ -0,0 +1,32 @@
+import esbuild from "esbuild";
+import { sassPlugin } from 'esbuild-sass-plugin';
+import path from 'path';
+import fs from 'fs';
+
+const dir = './SCSS/snippets';
+const files = fs.readdirSync(dir)
+
+const flags = process.argv;
+
+const options = {
+  production: flags.includes('production'),
+  snippet: flags.includes('snippet'),
+}
+
+function createSnippetArray () {
+  return files.map(file => {
+    return path.join(dir,file)
+  })
+}
+
+const outOptions = options.snippet ? {
+  outdir: "./snippets",
+} : { outfile: "./testing.css" };
+
+esbuild.build({
+    entryPoints: options.snippet ? createSnippetArray() : ["./SCSS/Spectrum.scss"],
+    ...outOptions,
+    minify: options.production,
+    plugins: [sassPlugin()],
+})
+
